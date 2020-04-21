d63c3c01cd86c1632e0456d9f16b0a90f979b753
Timo van de Put <timo.van.de.put@philips.com>
2020-04-21 13:32:37 +0200

    Extract base CLI command class

---
+++ lib/command/abstract_command.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:bompare/service/bom_service.dart';
+
+/// Base command to act upon provided scan result files.
+abstract class AbstractCommand extends Command {
+  static const option_reference = 'reference';
+  static const option_white_source = 'whitesource';
+  static const option_black_duck = 'blackduck';
+  static const option_output = 'out';
+  static const option_diff_only = 'diffOnly';
+
+  final BomService service;
+
+  AbstractCommand(this.service) {
+    argParser
+      ..addMultiOption(option_reference,
+          abbr: 'r',
+          help: 'Scan result in "reference" (JSON) format',
+          valueHelp: 'filename')
+      ..addMultiOption(option_white_source,
+          abbr: 'w',
+          help: 'Scan result in WhiteSource "inventory" (JSON) format',
+          valueHelp: 'filename')
+      ..addMultiOption(option_black_duck,
+          abbr: 'b',
+          help: 'Scan result in Black Duck "report" (ZIP/directory) format',
+          valueHelp: 'filename or directory')
+      ..addOption(option_output,
+          abbr: 'o',
+          help: 'Write detail report to (CSV) file',
+          valueHelp: 'filename')
+      ..addFlag(option_diff_only,
+          help: 'Only output diff lines in output file');
+  }
+
+  @override
+  Future<void> run() async {
+    await _loadScanResults();
+
+    return execute();
+  }
+
+  Future<void> _loadScanResults() async => await Future.wait([
+        _loadTypedResults(option_reference, ScannerType.reference),
+        _loadTypedResults(option_white_source, ScannerType.white_source),
+        _loadTypedResults(option_black_duck, ScannerType.black_duck),
+      ]);
+
+  Future<void> _loadTypedResults(String option, ScannerType type) =>
+      Future.forEach(argResults[option],
+          (filename) => service.loadResult(type, File(filename)));
+
+  Future<void> execute();
+}
--- lib/command/bom_command.dart
+++ lib/command/bom_command.dart
@@ -1,39 +1,13 @@
 import 'dart:io';
 
-import 'package:args/command_runner.dart';
 import 'package:bompare/service/bom_service.dart';
 
-class BomCommand extends Command {
-  static const option_reference = 'reference';
-  static const option_white_source = 'whitesource';
-  static const option_black_duck = 'blackduck';
-  static const option_output = 'out';
-  static const option_diff_only = 'diffOnly';
+import 'abstract_command.dart';
+
+class BomCommand extends AbstractCommand {
   static const command = 'bom';
 
-  final BomService service;
-
-  BomCommand(this.service) {
-    argParser
-      ..addMultiOption(option_reference,
-          abbr: 'r',
-          help: 'Scan result in "reference" (JSON) format',
-          valueHelp: 'filename')
-      ..addMultiOption(option_white_source,
-          abbr: 'w',
-          help: 'Scan result in WhiteSource "inventory" (JSON) format',
-          valueHelp: 'filename')
-      ..addMultiOption(option_black_duck,
-          abbr: 'b',
-          help: 'Scan result in Black Duck "report" (ZIP/directory) format',
-          valueHelp: 'filename or directory')
-      ..addOption(option_output,
-          abbr: 'o',
-          help: 'Write detail report to (CSV) file',
-          valueHelp: 'filename')
-      ..addFlag(option_diff_only,
-          help: 'Only output diff lines in output file');
-  }
+  BomCommand(BomService service) : super(service);
 
   @override
   String get name => command;
@@ -43,17 +17,11 @@ class BomCommand extends Command {
       'Analyze the BOM differences between selected scanners';
 
   @override
-  Future<void> run() async {
-    await Future.wait([
-      _loadTypedResults(option_reference, ScannerType.reference),
-      _loadTypedResults(option_white_source, ScannerType.white_source),
-      _loadTypedResults(option_black_duck, ScannerType.black_duck),
-    ]);
-
-    final file = (argResults[option_output] != null)
-        ? File(argResults[option_output])
+  Future<void> execute() async {
+    final file = (argResults[AbstractCommand.option_output] != null)
+        ? File(argResults[AbstractCommand.option_output])
         : null;
-    final diffOnly = argResults[option_diff_only];
+    final diffOnly = argResults[AbstractCommand.option_diff_only];
 
     final results =
         await service.compareResults(bomFile: file, diffOnly: diffOnly);
@@ -64,10 +32,5 @@ class BomCommand extends Command {
           '${result.additional} extra, '
           '${result.missing} missing');
     });
-  }
-
-  Future<void> _loadTypedResults(String option, ScannerType type) {
-    return Future.forEach(argResults[option],
-        (filename) => service.loadResult(type, File(filename)));
   }
 }
