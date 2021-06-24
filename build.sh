3636d6cbe7b17642fd1d628b703582aff0fdfcd2
Slave Mitev <slave.mitev@philips.com>
2021-06-24 23:00:35 +0200

    generate coverage badge

--- build.sh
+++ build.sh
@@ -17,6 +17,13 @@ if hash format_coverage >/dev/null && hash genhtml >/dev/null; then
   format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
   genhtml -o coverage coverage/lcov.info
   echo "Coverage report is found in /coverage/index.html"
+
+  if hash flutter_coverage_badge >/dev/null; then
+    flutter_coverage_badge
+    echo "Coverage badge generated."
+  else
+    echo "(Install dart package:flutter_coverage_badge globally to generate coverage badge.)"
+  fi
 else
   echo "(Install dart package:coverage globally and genhtml to generate a full coverage overview.)"
 fi
