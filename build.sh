683363d2b9ef0b07a1973db75a71ebe836f6caea
Timo van de Put <timo.van.de.put@philips.com>
2021-07-06 15:17:15 +0200

    Update coverage reporting to latest Dart version
    Running "Pub" from within Dart is now deprecated, so the build script was
    broken.
    
    - Remove coverage badge generation.

--- build.sh
+++ build.sh
@@ -11,21 +11,14 @@ dart analyze "test"
 dart format "bin" "lib" "test"
 
 # Calculate coverage by unit tests
-dart run test test --coverage coverage
+dart run test --coverage coverage
 
-if hash format_coverage >/dev/null && hash genhtml >/dev/null; then
-  format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
+if type format_coverage && type genhtml &> /dev/null ; then
+  dart run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
   genhtml -o coverage coverage/lcov.info
-  echo "Coverage report is found in /coverage/index.html"
-
-  if hash flutter_coverage_badge >/dev/null; then
-    flutter_coverage_badge
-    echo "Coverage badge generated."
-  else
-    echo "(Install dart package:flutter_coverage_badge globally to generate coverage badge.)"
-  fi
+  echo "Coverage report is found in ./coverage/index.html"
 else
-  echo "(Install dart package:coverage globally and genhtml to generate a full coverage overview.)"
+  echo "(Install dart package:coverage globally and lcov (genhtml) to generate a full coverage overview.)"
 fi
 
 # Build command line executable
